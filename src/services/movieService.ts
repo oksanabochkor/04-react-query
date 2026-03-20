306888c77ba034836dbf018ec193f6b9740f91c5
Oksana Bochkor <oksanabockor785@gmail.com>
2026-03-20 16:47:08 +0200

    hmw

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,7 +1,14 @@
-import axios from 'axios';
-import type { MoviesResponse } from '../types/movie';
+import axios from "axios";
+import type { Movie } from "../types/movie";
 
-const BASE_URL = 'https://api.themoviedb.org/3';
+
+interface MoviesResponse {
+  page: number;
+  results: Movie[];
+  total_pages: number;
+}
+
+const BASE_URL = "https://api.themoviedb.org/3";
 
 const token = import.meta.env.VITE_TMDB_TOKEN;
 
@@ -16,7 +23,7 @@ export const fetchMovies = async (
   query: string,
   page: number
 ): Promise<MoviesResponse> => {
-  const response = await instance.get('/search/movie', {
+  const response = await instance.get<MoviesResponse>("/search/movie", {
     params: {
       query,
       page,
